a7457c45616b0b34e2bd9366b43172cbb547e775
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-04-02 04:42:33 +0100

    Setup context manager, and autologin

--- frontend/Screens/HomeScreen.js
+++ frontend/Screens/HomeScreen.js
@@ -1,88 +1,18 @@
-import React, { useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  FlatList,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
-const data = [
-  {
-    id: '1',
-    name: 'Product 1',
-    carbonFootprint: 5.0,
-    imageURL:
-      'https://images.unsplash.com/photo-1569738287709-043a6b8f6f8a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VucmlzZWFyY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
-    quantity: 3,
-  },
-  {
-    id: '2',
-    name: 'Product 2',
-    carbonFootprint: 7.5,
-    imageURL:
-      'https://cdn.pixabay.com/photo/2016/03/27/18/54/technology-1283624_960_720.jpg',
-    quantity: 6,
-  },
-  {
-    id: '3',
-    name: 'Product 3',
-    carbonFootprint: 3.2,
-    imageURL:
-      'https://cdn.pixabay.com/photo/2017/08/07/14/53/aircraft-2603167_960_720.jpg',
-    quantity: 1,
-  },
-];
-
-const Item = ({ name, carbonFootprint, imageURL, quantity, index }) => {
-  const backgroundColor = index % 2 === 0 ? '#f2f2f2' : '#ffffff';
-  const totalCarbonFootprint = (carbonFootprint * quantity).toFixed(2);
-  return (
-    <View style={[styles.itemContainer, { backgroundColor }]}>
-      <Image source={{ uri: imageURL }} style={styles.itemImage} />
-      <View style={styles.itemDetails}>
-        <Text style={styles.itemName}>{name}</Text>
-        <Text style={styles.itemCarbonFootprint}>
-          Carbon Footprint: {carbonFootprint.toFixed(2)}
-        </Text>
-        <Text style={styles.itemCarbonFootprint}>Quantity: {quantity}</Text>
-      </View>
-      <Text style={styles.itemName}>
-        {totalCarbonFootprint}{' '}
-        <Text style={styles.itemCarbonFootprint}>kgco2</Text>
-      </Text>
-    </View>
-  );
-};
-
-export default function HomeScreen() {
-  const navigation = useNavigation();
-  const [items, setItems] = useState(data);
-
-  const handleScanPress = () => {
-    navigation.navigate('Scan');
-  };
+import { StyleSheet, View, Button } from "react-native";
+import Product from "../components/Product";
 
   return (
     <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Text style={styles.headingText}>Name</Text>
-        <Text style={styles.headingText}>Total</Text>
-      </View>
-      <FlatList
-        data={items}
-        renderItem={({ item, index }) => (
-          <Item
-            name={item.name}
-            carbonFootprint={item.carbonFootprint}
-            imageURL={item.imageURL}
-            quantity={item.quantity}
-            index={index}
-          />
-        )}
-        keyExtractor={(item) => item.id}
+      {route.params && (
+        <Product
+          name={route.params.name}
+          imageURL={route.params.imageURL}
+          carbonFootprint={route.params.carbonFootprint}
+        />
+      )}
+      <Button
+        title="Scan Product"
+        onPress={() => navigation.navigate("Scan")}
       />
       <TouchableOpacity style={styles.scanButton} onPress={handleScanPress}>
         <Text style={styles.scanButtonText}>Scan Product</Text>
@@ -94,6 +24,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
   },
   headingContainer: {
--- frontend/authentication/firebaseConfig.js
+++ frontend/authentication/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { GoogleAuthProvider } from 'firebase/auth';
+
 import {
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
@@ -30,5 +30,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
-export const provider = new GoogleAuthProvider();
 export const auth = getAuth(app);
