50d5e33fa247230712611bad5264795591b1e055
AlexTClarke <119004428+AlexTClarke@users.noreply.github.com>
2023-04-02 04:56:48 +0100

    Add icons to bottom nav bar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -2,14 +2,40 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen';
 import EmptyScreen from '../Screens/EmptyScreen';
 import LoginScreen from '../Screens/LoginScreen';
+import { Ionicons } from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 
 export default function HomeTabs() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name='Home' component={HomeScreen} />
-      <Tab.Screen name='Login' component={LoginScreen} />
-      <Tab.Screen name='Notifications' component={EmptyScreen} />
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='home' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Login'
+        component={LoginScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='log-in' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Notifications'
+        component={EmptyScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='notifications' size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
