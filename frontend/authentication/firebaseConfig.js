f9535c2bb97d7629373d4e047bf7856c133acf7c
Kevin Roman <44275689+Kevin-Roman@users.noreply.github.com>
2023-04-02 01:27:30 +0100

    Update firebase config

--- frontend/authentication/firebaseConfig.js
+++ frontend/authentication/firebaseConfig.js
@@ -2,6 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { GoogleAuthProvider } from 'firebase/auth';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from '@env';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,13 +18,13 @@ import { GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBTlOTBA_IljcbNlImhrTWi2F9VSMB7zGY',
-  authDomain: 'bathhack-23.firebaseapp.com',
-  projectId: 'bathhack-23',
-  storageBucket: 'bathhack-23.appspot.com',
-  messagingSenderId: '767853222474',
-  appId: '1:767853222474:web:c19751ccb4a902ce5524b3',
-  measurementId: 'G-WNFKG9H3B2',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
